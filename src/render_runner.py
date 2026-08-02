f9275a68f271904f9b4f55679a4ab90ad3f6632c
onyekaisagba-cloud <onyeka.isagba@gmail.com>
2026-08-02 10:21:49 -0400

    Update render_runner.py

--- src/render_runner.py
+++ src/render_runner.py
@@ -27,6 +27,11 @@ class HealthCheckHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"Onyx Deal Engine Runner Active")
 
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
 def start_health_server():
     port = int(os.environ.get("PORT", 8080))
     server = HTTPServer(("0.0.0.0", port), HealthCheckHandler)
